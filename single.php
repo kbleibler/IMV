6dd3cafce87838ddb355fe3389123a28cbf1aeb6
kbleibler <katja.bleibler@stud.fhgr.ch>
2023-12-08 19:21:02 +0100

    2. final upload aller dokumente

--- single.php
+++ single.php
@@ -3,11 +3,9 @@
    <div id="main">
        
       <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-         <h2><a href="<?php the_permalink() ?>"><?php the_title(); ?></a></h2>
+         <h2><?php the_title(); ?></h2>
          <div id="meta">
             <p>erstellt am: <?php the_date('d.m.Y'); ?> | 
-            von: <?php the_author(); ?> | 
-            Kategorie(n): <?php the_category(', '); ?></p>
          </div>
          <div class="entry">
             <?php the_content(); ?>
